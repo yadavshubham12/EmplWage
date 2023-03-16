00dcc3679e6ce11e184cd4a23f110e5eb0bb2efa
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-03-16 15:15:02 +0530

    ADDED: Print to Half Day Wage

--- src/com/blz/EmpWage/EmployeeWage.java
+++ src/com/blz/EmpWage/EmployeeWage.java
@@ -3,16 +3,28 @@ package com.blz.EmpWage;
 public class EmployeeWage {
     public static void main(String[] args) {
 
+        int FullTime = 2;
+        int HalfTime = 1;
         int fullDayHr = 8;
+        int halfDayHr = 4;
         int wagePerHr = 20;
         int dailyWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
-        if (empCheck == 1){
-            System.out.println("Employee is present");
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == FullTime){
+            fullDayHr = 8;
+            System.out.println("Employee is Full Day present");
             dailyWage = wagePerHr * fullDayHr;
+            System.out.println("Daily Wage of Employee is " + dailyWage);
+        }
+        else if (empCheck == HalfTime){
+            halfDayHr = 4;
+            System.out.println("Employee is Half Day Present");
+            dailyWage = wagePerHr * halfDayHr;
+            System.out.println("Daily Wage of Employee is " + dailyWage);
         }
         else {
-            System.out.println("Employee is absent");
+
+            System.out.println("Employee is absent");;
         }
         System.out.println(" Employee Wage " + dailyWage);
     }
