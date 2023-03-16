47d3d5302f668336589e1fac8cc1c81733ad1001
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-03-16 15:37:18 +0530

    ADDED: Print to Switch Case Method

--- src/com/blz/EmpWage/EmployeeWage.java
+++ src/com/blz/EmpWage/EmployeeWage.java
@@ -9,23 +9,23 @@ public class EmployeeWage {
         int halfDayHr = 4;
         int wagePerHr = 20;
         int dailyWage = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == FullTime){
-            fullDayHr = 8;
-            System.out.println("Employee is Full Day present");
-            dailyWage = wagePerHr * fullDayHr;
-            System.out.println("Daily Wage of Employee is " + dailyWage);
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck) {
+            case 1: {
+                System.out.println("Employee is Full Day Present");
+                empCheck = wagePerHr * fullDayHr;
+            }
+            break;
+            case 2: {
+                System.out.println("Employee is Half Day Present");
+                empCheck = wagePerHr * halfDayHr;
+            }
+            break;
+            case 3: {
+                System.out.println("Employee is absent");
+            }
+            System.out.println(" Daily Wage of Employee is " + dailyWage);
+            System.out.println(" Employee Wage " +dailyWage);
+            }
         }
-        else if (empCheck == HalfTime){
-            halfDayHr = 4;
-            System.out.println("Employee is Half Day Present");
-            dailyWage = wagePerHr * halfDayHr;
-            System.out.println("Daily Wage of Employee is " + dailyWage);
         }
-        else {
-
-            System.out.println("Employee is absent");;
-        }
-        System.out.println(" Employee Wage " + dailyWage);
-    }
-}
