ba90c8dfcfaabce17fbe426d885f5093a254ea5e
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-03-16 14:24:21 +0530

    ADDED: Print to Daily Wage of Employee

--- src/com/blz/EmpWage/EmployeeWage.java
+++ src/com/blz/EmpWage/EmployeeWage.java
@@ -2,12 +2,18 @@ package com.blz.EmpWage;
 
 public class EmployeeWage {
     public static void main(String[] args) {
+
+        int fullDayHr = 8;
+        int wagePerHr = 20;
+        int dailyWage = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
         if (empCheck == 1){
             System.out.println("Employee is present");
+            dailyWage = wagePerHr * fullDayHr;
         }
         else {
             System.out.println("Employee is absent");
         }
+        System.out.println(" Employee Wage " + dailyWage);
     }
 }
